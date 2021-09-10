269fcd05db856d5140cdc023d8b0aa3f33d8ec79
UrielNunez <Devuriel1@gmail.com>
2021-09-10 18:35:20 -0500

    COMIT DE PRUEBA

--- src/main/java/datos/Conexion.java
+++ src/main/java/datos/Conexion.java
@@ -9,11 +9,11 @@ import java.sql.*;
  */
 public class Conexion {
     //GENERAR LAS CONSTANTES DE CREDENCIALES PARA LA BD
-    /*private static final String JDBC_URL = "jdbc:mysql://localhost:3306/prueba?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/prueba?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASSWORD = "admin123";
     
-    public static Connection getConnection(){
+    /*public static Connection getConnection(){
         return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD );
     }*/
     
